b1e3927163f59f93f0ec5c0db6830dfb6b8de160
Ilya Khlyustov <ik251200@bk.ru>
2022-06-13 20:36:14 +0300

    fixed code style

--- modules/butescu_livenstein_distatnce/application/application.cpp
+++ modules/butescu_livenstein_distatnce/application/application.cpp
@@ -7,7 +7,7 @@
 #include "include/levenshtein_distance_application.h"
 
 int main(int argc, const char** argv) {
-    Application app;
+    LevenshteinDistanceApp app;
     std::string output = app(argc, argv);
     printf("%s\n", output.c_str());
     return 0;
